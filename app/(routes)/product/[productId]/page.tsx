a40c8030ca362955bb9e1c5c128e9592648f23ca
Rohan Rathod <rohan.rthd.58@gmail.com>
2023-08-20 12:52:57 +0530

    Add Product Info

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 import Container  from "@/components/ui/container";
 import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 
 interface ProductPageProps{
     params:{
@@ -25,8 +26,7 @@ const ProductPage: React.FC<ProductPageProps> =  async ({
                         <Gallery images={product.images}/>
         
                         <div className="mt-10 px-4 sm:mt--16 sm:px-0 lg:mt-0">
-                            {/* Info */}
-                            Info
+                           <Info data={product}/>
                         </div>
                     </div>
                 </div>
