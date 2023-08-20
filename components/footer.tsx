0df8221380f3c9cdaf459fef7a48f568645af25a
Rohan Rathod <rohan.rthd.58@gmail.com>
2023-08-20 12:17:04 +0530

    Add Front Page
    Front page with product list and related items

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return(
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-cs text-black">
+                    &copy; 2023 Rohan Rathod, All rights reserved.
+
+                </p>
+
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
